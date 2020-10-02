6fae620e043e9b99e30585a52eb7e53c16bd6b8b
jeben-hub <abondaronok@gmail.com>
2020-10-03 01:07:54 +0300

    feat min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+function isValid(array) {
+  return Array.isArray(array) && array.length > 0;
+}
+
 
 exports.min = function min (array) {
-  return 0;
+  if (!isValid(array)) return 0;
+  array.sort(function(a, b) {
+    return b - a;
+  });
+  return array.pop();
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!isValid(array)) return 0;
+  array.sort(function(a, b) {
+    return a - b;
+  });
+  return array.pop();
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!isValid(array)) return 0;
+  return array.reduce(function(a, b) {
+    return a + b;
+  }) / array.length;
 }
